5621c215d0de9afc5f044855469939f194d870f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-10 12:19:29 +0100

    Tests for the core module

---
+++ test/chainring_service/core_test.clj
@@ -0,0 +1,7 @@
+(ns chainring-service.core-test
+  (:require [clojure.test :refer :all]
+            [chainring-service.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
