163728166a4425ab600e2ccee5e812d1718151bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-14 00:40:26 +0100

    Updated test data - SAP numbers everywhere

--- database/test_data.sql
+++ database/test_data.sql
@@ -1,9 +1,9 @@
-insert into project(id, name) values(1, 'Testovaci projekt 1');
+insert into project(id, sap, name) values(1, 'SAP10000', 'Testovaci projekt 1');
 
-insert into building(id, project, name) values(1, 1, 'Budova 1');
-insert into building(id, project, name) values(2, 1, 'Budova 2');
+insert into building(id, project, sap, name) values(1, 1, 'SAP11000', 'Budova 1');
+insert into building(id, project, sap, name) values(2, 1, 'SAP12000', 'Budova 2');
 
-insert into drawing(id, building, name) values(1, 1, 'Podlazi 1');
-insert into drawing(id, building, name) values(2, 1, 'Podlazi 2');
-insert into drawing(id, building, name) values(3, 1, 'Podlazi 3');
+insert into drawing(id, building, sap, name) values(1, 1, 'SAP11100', 'Podlazi 1');
+insert into drawing(id, building, sap, name) values(2, 1, 'SAP11110', 'Podlazi 2');
+insert into drawing(id, building, sap, name) values(3, 1, 'SAP11120', 'Podlazi 3');
 
