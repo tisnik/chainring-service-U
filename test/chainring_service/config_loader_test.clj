f04ff7bc52d6af82303e29d73453862ac6afe2bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-22 17:48:50 +0100

    Stub for the test module config_loader

---
+++ test/chainring_service/config_loader_test.clj
@@ -0,0 +1,8 @@
+(ns chainring-service.config-loader-test
+  (:require [clojure.test :refer :all]
+            [chainring-service.config-loader :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
