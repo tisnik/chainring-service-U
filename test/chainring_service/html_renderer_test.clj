af83989cef619ba8ec51e2cd0248caef573c7834
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-23 15:10:09 +0100

    Stub for html_renderer_test

---
+++ test/chainring_service/html_renderer_test.clj
@@ -0,0 +1,7 @@
+(ns chainring-service.html-renderer-test
+  (:require [clojure.test :refer :all]
+            [chainring-service.html-renderer :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
