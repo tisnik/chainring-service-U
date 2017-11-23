5dcfa63ffc11975c05b7dbb0ad97a6e354a4fd16
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-23 15:10:51 +0100

    Stub for rest_api_test

---
+++ test/chainring_service/rest_api_test.clj
@@ -0,0 +1,7 @@
+(ns chainring-service.rest-api-test
+  (:require [clojure.test :refer :all]
+            [chainring-service.rest-api :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
