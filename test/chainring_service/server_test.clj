4c6f1604ddaf7e7df353fbd043bd6616f11da8a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-23 15:13:42 +0100

    Stub for server_test

---
+++ test/chainring_service/server_test.clj
@@ -0,0 +1,7 @@
+(ns chainring-service.server-test
+  (:require [clojure.test :refer :all]
+            [chainring-service.server :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
