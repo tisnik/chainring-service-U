b25c36596ae1ae044e9472658979249e04e7b2f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-23 15:09:24 +0100

    Stub for db_spec_test

---
+++ test/chainring_service/db_spec_test.clj
@@ -0,0 +1,7 @@
+(ns chainring-service.db-interface-test
+  (:require [clojure.test :refer :all]
+            [chainring-service.db-interface :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
