78343f53122662821203b5b65811a897f23f475c
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-24 11:43:13 +0100

    Removed test example + added common functions used by tests

--- test/chainring_service/config_loader_test.clj
+++ test/chainring_service/config_loader_test.clj
@@ -1,8 +1,29 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns chainring-service.config-loader-test
   (:require [clojure.test :refer :all]
             [chainring-service.config-loader :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for functions existence
+;
 
