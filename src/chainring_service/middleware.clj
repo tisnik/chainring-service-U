e295590eebe6d61ffd73b5df55fb5b13fc8d6f87
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-05 00:21:59 +0100

    Middleware processing

---
+++ src/chainring_service/middleware.clj
@@ -0,0 +1,25 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns chainring-service.middleware
+    "Definition(s) of middleware used by this app with the cooperation with Clojure Ring.")
+
+(require '[clojure.tools.logging   :as log])
+
+(defn inject-configuration
+    "Inject configuration structure into the request parameter.
+     It means that the process request -> emender-waive -> response could be implemented
+     in a functional way."
+    [handler configuration]
+    (fn [request]
+        (handler (assoc request :configuration configuration))))
+
