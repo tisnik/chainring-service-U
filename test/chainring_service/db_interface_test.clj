6952fefc16c474aad1efb630f210ec3af477f2fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-07 14:10:30 +0100

    Common function to be used by tests

--- test/chainring_service/db_interface_test.clj
+++ test/chainring_service/db_interface_test.clj
@@ -2,7 +2,16 @@
   (:require [clojure.test :refer :all]
             [chainring-service.db-interface :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for functions existence
+;
 
