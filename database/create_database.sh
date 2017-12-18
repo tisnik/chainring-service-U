d9cb519d16e3b5ee3385e1b521e2f32673798082
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-18 11:46:15 +0100

    Renamed SQL script with catalogs

--- database/create_database.sh
+++ database/create_database.sh
@@ -3,6 +3,6 @@
 DATABASE=chainring.db
 
 cat schema.sql | sqlite3 ../${DATABASE}
-cat catalog.sql | sqlite3 ../${DATABASE}
+cat catalogs.sql | sqlite3 ../${DATABASE}
 cat test_data.sql | sqlite3 ../${DATABASE}
 
