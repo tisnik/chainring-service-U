acd886cdba3016658fd77237f7e977076895e236
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-08 00:10:11 +0100

    Two new configuration options + code to load/set them

--- src/chainring_service/config.clj
+++ src/chainring_service/config.clj
@@ -3,12 +3,18 @@
 (require '[clj-utils.utils :as utils])
 (require '[config-loader.config-loader :as config-loader])
 
+(defn full-prefix
+    "Construct full prefix for REST API endpoints (including API version)."
+    [configuration]
+    (str (-> configuration :api :prefix) "/" (-> configuration :api :version)))
+
 (defn update-configuration
     "Update selected items in the configuration structure."
     [configuration]
     (-> configuration
-        (update-in [:config :verbose]                    utils/parse-boolean)
-        (update-in [:config :pretty-print]               utils/parse-boolean)))
+        (update-in [:config :verbose]        utils/parse-boolean)
+        (update-in [:config :pretty-print]   utils/parse-boolean)
+        (assoc-in  [:api    :full-prefix]    (full-prefix configuration))))
 
 (defn load-configuration-from-ini
     "Load configuration from the provided INI file and perform conversions
@@ -27,3 +33,27 @@
      HTTP request structure (middleware can be used to pass config into it)."
     [request]
     (-> request :configuration :api :prefix))
+
+(defn get-api-version
+    "Read prefix for API calls from the configuration passed via
+     HTTP request structure (middleware can be used to pass config into it)."
+    [request]
+    (-> request :configuration :api :version))
+
+(defn get-version
+    "Read the version of this service from the configuration passed via
+     HTTP request structure (middleware can be used to pass config into it)."
+    [request]
+    (-> request :configuration :info :version))
+
+(defn get-db-version
+    "Read the DB version of this service from the configuration passed via
+     HTTP request structure (middleware can be used to pass config into it)."
+    [request]
+    (-> request :configuration :info :db-version))
+
+(defn get-api-full-prefix
+    "Read the DB version of this service from the configuration passed via
+     HTTP request structure (middleware can be used to pass config into it)."
+    [request]
+    (-> request :configuration :api :full-prefix))
