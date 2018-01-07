85041bfeeab0727c954a654334b8bdf72a931be0
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-08 00:10:59 +0100

    Script to dump database should dump all tables

--- database/dump_database.sql
+++ database/dump_database.sql
@@ -1,6 +1,12 @@
 .output database_dump.sql
-.dump project
-.dump building
-.dump room
-.dump drawing
+.dump PROJECT
+.dump BUILDING
+.dump FLOOR
+.dump SAP_ROOM
+.dump ATTRIBUTE_TYPE
+.dump ROOM_ATTRIBUTE
+.dump DRAWING
+.dump DRAWING_RAW_DATA
+.dump DRAWING_ROOM
+.dump ROOM_VERTEX
 .quit
