74d18c089e1b5cf1213332ed1c6003223aad7373
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-19 00:19:06 +0100

    Templates for more pages: settings, db statistic, store settings etc.

--- src/chainring_service/html_renderer.clj
+++ src/chainring_service/html_renderer.clj
@@ -72,46 +72,103 @@
         [:body
             [:div {:class "container"}
                 (render-navigation-bar-section "/")
-                [:table {:class "table table-stripped table-hover" :style "width:auto"}
-                    [:tr [:th [:a {:href "project-list"} "Seznam projektů"]]
-                    ]
-                ]
+                [:a {:href "project-list" :class "btn btn-success" :role "button" :style "width:10em"} "Seznam projektů"]
                 [:div {:style "height: 10ex"}]
-                [:table {:class "table table-hover" :style "width:auto"}
-                    [:tr [:th "Další volby"]]
-                    [:tr [:td [:a {:href "settings"} "Nastavení"]]]
-                    [:tr [:td [:a {:href "db-stats"} "Stav databáze"]]]
-                ]
+                [:h3 "Další volby"]
+                [:a {:href "settings" :class "btn btn-default" :role "button" :style "width:10em"} "Nastavení"]
+                [:br]
+                [:br]
+                [:a {:href "db-stats" :class "btn btn-default" :role "button" :style "width:10em"} "Stav databáze"]
                 (render-html-footer)
             ] ; </div class="container">
         ] ; </body>
 ))
+
+(defn color-box
+    [html-color value]
+    [:a {:href "#" :onclick
+        (str "document.getElementById('selected-room-color').value='" value "';"
+             "document.getElementById('selected-room-color-code').value='" html-color "'")}
+        [:div {:class "color-box" :style (str "background-color:" html-color "; display:inline-block")}]])
 
 (defn render-settings-page
-    []
+    [user-id]
     (page/xhtml
         (render-html-header "/")
         [:body
             [:div {:class "container"}
                 (render-navigation-bar-section "/")
-                [:table {:class "table table-stripped table-hover" :style "width:auto"}
-                    [:tr [:th "Nastavení"]]
-                ]
+                [:h1 "Nastavení"]
+                (form/form-to [:post "store-settings"]
+                    [:input {:type "hidden" :name "selected-room-color-code" :id "selected-room-color-code" :value "xxx"}]
+                    [:fieldset {:class "form-group"}
+                        [:label {:for "user-id"} "ID uživatele"]
+                        [:input {:type "text" :class "form-control" :style "width:10em" :readonly "readonly" :id "user-id" :name "user-id" :value user-id}]
+                    ]
+                    [:fieldset {:class "form-group"}
+                        [:label {:for "resolution"} "Výchozí velikost půdorysu"]
+                        [:br]
+                        (form/drop-down "resolution" ["640x480" "800x600" "1024x768"])
+                    ]
+                    [:fieldset {:class "form-group"}
+                        [:label {:for "selected-room-color"} "Barva výplně vybrané místnosti"]
+                        [:br]
+                        (color-box "#ff0000" "červená")
+                        (color-box "#ff8000" "oranžová")
+                        (color-box "#ffff00" "žlutá")
+                        (color-box "#80ff80" "zelená")
+                        (color-box "#80ffff" "modrozelená")
+                        (color-box "#8080ff" "modrá")
+                        (color-box "#ff80ff" "fialová")
+                        (color-box "#a0a0a0" "šedá")
+                        (color-box "#ffffff" "bez výplně")
+                        [:input {:type "text" :class "form-control" :style "width:10em" :readonly "readonly" :id "selected-room-color" :name "selected-room-color" :value ""}]
+                    ]
+                    [:fieldset {:class "form-group"}
+                        [:label {:for "selected-room-color"} "Šířka obrysu vybrané místnosti"]
+                        [:br]
+                        (form/drop-down "pen-width" [["1 pixel" 1] ["2 pixely" 2] ["3 pixely" 3]])
+                    ]
+                    [:br]
+                    [:br]
+                    [:button {:type "submit" :class "btn btn-success"} "Uložit nastavení"]
+                )
+                [:br]
+                (form/form-to [:get "/"]
+                    [:button {:type "submit" :class "btn btn-primary"} "Zpět"]
+                )
                 (render-html-footer)
             ] ; </div class="container">
         ] ; </body>
 ))
 
+; (form/text-field {:size "40" :class "form-control" :placeholder "Search for word"} "word" (str word))
+; (form/drop-down "class" ["N/A" "Noun" "Verb" "Adjective" "Adverb" "Pronoun" "Preposition" "Conjunction" "Determiner" "Exclamation"])]]
+; (form/check-box "internal")]]
+
 (defn render-db-statistic-page
-    []
+    [db-stats]
     (page/xhtml
         (render-html-header "/")
         [:body
             [:div {:class "container"}
                 (render-navigation-bar-section "/")
+                [:h1 "Stav databáze"]
                 [:table {:class "table table-stripped table-hover" :style "width:auto"}
-                    [:tr [:th "Stav databáze"]]
+                    [:tr [:th "Tabulka"]                [:th "Počet záznamů"]]
+                    [:tr [:td "Areály"]                 [:td (:projects db-stats)]]
+                    [:tr [:td "Budovy"]                 [:td (:buildings db-stats)]]
+                    [:tr [:td "Podlaží"]                [:td (:floors db-stats)]]
+                    [:tr [:td "Místnosti (SAP)"]        [:td (:sap-rooms db-stats)]]
+                    [:tr [:td "Výkresy"]                [:td (:drawings db-stats)]]
+                    [:tr [:td "Data výkresů"]           [:td (:drawings-data db-stats)]]
+                    [:tr [:td "Místnosti na výkresech"] [:td (:drawing-rooms db-stats)]]
+                    [:tr [:td "Uživatelé"]              [:td (:users db-stats)]]
                 ]
+                [:br]
+                (form/form-to [:get "/"]
+                    [:button {:type "submit" :class "btn btn-primary"} "Zpět"]
+                )
                 (render-html-footer)
             ] ; </div class="container">
         ] ; </body>
@@ -255,11 +312,26 @@
         [:body
             [:div {:class "container"}
                 (render-navigation-bar-section "/")
-                [:div {:class "col-md-10"}
-                    [:h2 "Chyba či neočekávaný stav"]
-                    [:p {:class "alert alert-danger"} message]
-                    [:button {:class "btn btn-primary" :onclick "window.history.back()" :type "button"} "Zpět"]
-                ]
+                [:h1 "Chyba či neočekávaný stav"]
+                [:p {:class "alert alert-danger"} message]
+                [:button {:class "btn btn-primary" :onclick "window.history.back()" :type "button"} "Zpět"]
+                [:br][:br][:br][:br]
+                (render-html-footer)
+            ] ; </div class="container">
+        ] ; </body>
+))
+
+(defn render-store-settings-page
+    "Render an ino page with a 'back' button."
+    []
+    (page/xhtml
+        (render-html-header "/")
+        [:body
+            [:div {:class "container"}
+                (render-navigation-bar-section "/")
+                [:h1 "Nastavení"]
+                [:p {:class "alert alert-success"} "Změny byly úspěšně uloženy"]
+                [:button {:class "btn btn-primary" :onclick "window.history.back()" :type "button"} "Zpět"]
                 [:br][:br][:br][:br]
                 (render-html-footer)
             ] ; </div class="container">
