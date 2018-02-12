020f4b1f431eac8a3946269ce6ad810137d8fa95
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-12 08:53:11 +0100

    New module for vector drawing rendering

---
+++ src/chainring_service/vector_renderer.clj
@@ -0,0 +1,49 @@
+;
+;  (C) Copyright 2017, 2018  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns chainring-service.vector-renderer)
+
+(require '[ring.util.response    :as http-response])
+(require '[clojure.tools.logging :as log])
+
+(require '[chainring-service.http-utils :as http-utils])
+
+(defn send-drawing
+    [request mime-type extension]
+    [request]
+    (let [params       (:params request)
+          drawing-id   (get params "drawing-id")
+          drawing-name (get params "drawing-name")]
+          (log/info "Drawing ID:" drawing-id)
+          (cond drawing-id (try
+                               (let [drawing-name (format (str "%05d." extension) (Integer/parseInt drawing-id))]
+                                    (log/info "Drawing name:" drawing-name)
+                                    (http-utils/return-file "drawings" drawing-name mime-type))
+                               (catch Exception e
+                                   (log/error e)))
+                drawing-name (try
+                               (log/info "Drawing name:" drawing-name)
+                               (http-utils/return-file "drawings" drawing-name mime-type)
+                               (catch Exception e
+                                   (log/error e)))
+                :else nil)))
+
+(defn vector-drawing
+    "REST API handler for the /api/vector-drawing endpoint."
+    [request]
+    (send-drawing request "text/plain" "drw"))
+
+(defn vector-drawing-as-json
+    "REST API handler for the /api/vector-drawing-as-json endpoint."
+    [request]
+    (send-drawing request "application/json" "json"))
+
