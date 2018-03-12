eb71b8ef3805e863b4725f96bd62cb2286e5a68b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-12 09:36:54 +0100

    Updated formatting (minor)

--- src/chainring_service/config.clj
+++ src/chainring_service/config.clj
@@ -12,25 +12,30 @@
 
 
 (ns chainring-service.config
-    "Configuration loader")
+    "Configuration loader and parser.")
+
 
 (require '[clj-utils.utils :as utils])
 (require '[config-loader.config-loader :as config-loader])
+
 
 (defn full-prefix
     "Construct full prefix for REST API endpoints (including API version)."
     [configuration]
     (str (-> configuration :api :prefix) "/" (-> configuration :api :version)))
 
+
 (defn update-configuration
-    "Update selected items in the configuration structure."
+    "Update all selected items in the configuration structure.
+     Other items are stored with string values."
     [configuration]
     (-> configuration
-        (update-in [:config :verbose]          utils/parse-boolean)
-        (update-in [:config :pretty-print]     utils/parse-boolean)
+        (update-in [:config   :verbose]        utils/parse-boolean)
+        (update-in [:config   :pretty-print]   utils/parse-boolean)
         (update-in [:renderer :default_width]  utils/parse-int)
         (update-in [:renderer :default_height] utils/parse-int)
-        (assoc-in  [:api    :full-prefix]      (full-prefix configuration))))
+        (assoc-in  [:api      :full-prefix]    (full-prefix configuration))))
+
 
 (defn load-configuration-from-ini
     "Load configuration from the provided INI file and perform conversions
@@ -39,10 +44,12 @@
     (-> (config-loader/load-configuration-file ini-file-name)
         update-configuration))
 
+
 (defn pretty-print?
-    "Read the pretty-print settings (it is used for JSON output etc.)"
+    "Read the pretty-print settings (it is used mainly for JSON output etc.)"
     [request]
     (-> request :configuration :config :pretty-print))
+
 
 (defn get-api-prefix
     "Read prefix for API calls from the configuration passed via
@@ -50,11 +57,13 @@
     [request]
     (-> request :configuration :api :prefix))
 
+
 (defn get-api-version
     "Read prefix for API calls from the configuration passed via
      HTTP request structure (middleware can be used to pass config into it)."
     [request]
     (-> request :configuration :api :version))
+
 
 (defn get-version
     "Read the version of this service from the configuration passed via
@@ -62,17 +71,20 @@
     [request]
     (-> request :configuration :info :version))
 
+
 (defn get-db-version
     "Read the DB version of this service from the configuration passed via
      HTTP request structure (middleware can be used to pass config into it)."
     [request]
     (-> request :configuration :info :db-version))
 
+
 (defn get-api-full-prefix
     "Read the DB version of this service from the configuration passed via
      HTTP request structure (middleware can be used to pass config into it)."
     [request]
     (-> request :configuration :api :full-prefix))
+
 
 (defn verbose?
     "Read the verbose settings (it is used for all outputs that does not went into logs)"
