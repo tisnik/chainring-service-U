390e082a77ba23e3db1c51514b06cd9c38dc3d22
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-04 08:29:32 +0200

    More tests for the 'server' module

--- test/chainring_service/server_test.clj
+++ test/chainring_service/server_test.clj
@@ -15,6 +15,12 @@
 ; Tests for functions existence
 ;
 
+(deftest test-get-user-id-existence
+    "Check that the chainring-service.server/get-user-id definition exists."
+    (testing "if the chainring-service.server/get-user-id definition exists."
+        (is (callable? 'chainring-service.server/get-user-id))))
+
+
 (deftest test-finish-processing-existence
     "Check that the chainring-service.server/finish-processing definition exists."
     (testing "if the chainring-service.server/finish-processing definition exists."
@@ -27,10 +33,76 @@
         (is (callable? 'chainring-service.server/process-front-page))))
 
 
+(deftest test-process-settings-page-existence
+    "Check that the chainring-service.server/process-settings-page definition exists."
+    (testing "if the chainring-service.server/process-settings-page definition exists."
+        (is (callable? 'chainring-service.server/process-settings-page))))
+
+
+(deftest test-process-store-settings-page-existence
+    "Check that the chainring-service.server/process-store-settings-page definition exists."
+    (testing "if the chainring-service.server/process-store-settings-page definition exists."
+        (is (callable? 'chainring-service.server/process-store-settings-page))))
+
+
+(deftest test-process-db-statistic-page-existence
+    "Check that the chainring-service.server/process-db-statistic-page definition exists."
+    (testing "if the chainring-service.server/process-db-statistic-page definition exists."
+        (is (callable? 'chainring-service.server/process-db-statistic-page))))
+
+
+(deftest test-process-drawings-statistic-page-existence
+    "Check that the chainring-service.server/process-drawings-statistic-page definition exists."
+    (testing "if the chainring-service.server/process-drawings-statistic-page definition exists."
+        (is (callable? 'chainring-service.server/process-drawings-statistic-page))))
+
+
+(deftest test-process-drawings-list-existence
+    "Check that the chainring-service.server/process-drawings-list definition exists."
+    (testing "if the chainring-service.server/process-drawings-list definition exists."
+        (is (callable? 'chainring-service.server/process-drawings-list))))
+
+
+(deftest test-process-json-list-existence
+    "Check that the chainring-service.server/process-json-list definition exists."
+    (testing "if the chainring-service.server/process-json-list definition exists."
+        (is (callable? 'chainring-service.server/process-json-list))))
+
+
+(deftest test-process-binary-list-existence
+    "Check that the chainring-service.server/process-binary-list definition exists."
+    (testing "if the chainring-service.server/process-binary-list definition exists."
+        (is (callable? 'chainring-service.server/process-binary-list))))
+
+
 (deftest test-process-project-list-page-existence
     "Check that the chainring-service.server/process-project-list-page definition exists."
     (testing "if the chainring-service.server/process-project-list-page definition exists."
         (is (callable? 'chainring-service.server/process-project-list-page))))
+
+
+(deftest test-process-project-info-page-existence
+    "Check that the chainring-service.server/process-project-info-page definition exists."
+    (testing "if the chainring-service.server/process-project-info-page definition exists."
+        (is (callable? 'chainring-service.server/process-project-info-page))))
+
+
+(deftest test-process-building-info-page-existence
+    "Check that the chainring-service.server/process-building-info-page definition exists."
+    (testing "if the chainring-service.server/process-building-info-page definition exists."
+        (is (callable? 'chainring-service.server/process-building-info-page))))
+
+
+(deftest test-process-floor-info-page-existence
+    "Check that the chainring-service.server/process-floor-info-page definition exists."
+    (testing "if the chainring-service.server/process-floor-info-page definition exists."
+        (is (callable? 'chainring-service.server/process-floor-info-page))))
+
+
+(deftest test-process-room-list-existence
+    "Check that the chainring-service.server/process-room-list definition exists."
+    (testing "if the chainring-service.server/process-room-list definition exists."
+        (is (callable? 'chainring-service.server/process-room-list))))
 
 
 (deftest test-process-project-page-existence
@@ -45,16 +117,52 @@
         (is (callable? 'chainring-service.server/process-building-page))))
 
 
+(deftest test-process-floor-page-existence
+    "Check that the chainring-service.server/process-floor-page definition exists."
+    (testing "if the chainring-service.server/process-floor-page definition exists."
+        (is (callable? 'chainring-service.server/process-floor-page))))
+
+
+(deftest test-process-drawing-preview-page-existence
+    "Check that the chainring-service.server/process-drawing-preview-page definition exists."
+    (testing "if the chainring-service.server/process-drawing-preview-page definition exists."
+        (is (callable? 'chainring-service.server/process-drawing-preview-page))))
+
+
+(deftest test-process-raster-preview-page-existence
+    "Check that the chainring-service.server/process-raster-preview-page definition exists."
+    (testing "if the chainring-service.server/process-raster-preview-page definition exists."
+        (is (callable? 'chainring-service.server/process-raster-preview-page))))
+
+
 (deftest test-process-drawing-page-existence
     "Check that the chainring-service.server/process-drawing-page definition exists."
     (testing "if the chainring-service.server/process-drawing-page definition exists."
         (is (callable? 'chainring-service.server/process-drawing-page))))
 
 
+(deftest test-get-api-part-from-uri-existence
+    "Check that the chainring-service.server/get-api-part-from-uri definition exists."
+    (testing "if the chainring-service.server/get-api-part-from-uri definition exists."
+        (is (callable? 'chainring-service.server/get-api-part-from-uri))))
+
+
+(deftest test-get-api-command-existence
+    "Check that the chainring-service.server/get-api-command definition exists."
+    (testing "if the chainring-service.server/get-api-command definition exists."
+        (is (callable? 'chainring-service.server/get-api-command))))
+
+
 (deftest test-api-call-handler-existence
     "Check that the chainring-service.server/api-call-handler definition exists."
     (testing "if the chainring-service.server/api-call-handler definition exists."
         (is (callable? 'chainring-service.server/api-call-handler))))
+
+
+(deftest test-uri->file-name-existence
+    "Check that the chainring-service.server/uri->file-name definition exists."
+    (testing "if the chainring-service.server/uri->file-name definition exists."
+        (is (callable? 'chainring-service.server/uri->file-name))))
 
 
 (deftest test-gui-call-handler-existence
