cbca050448d724149f107bc0154057562603123b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-04 08:29:16 +0200

    More tests for the 'vector-drawing-test' module

--- test/chainring_service/vector_drawing_test.clj
+++ test/chainring_service/vector_drawing_test.clj
@@ -15,3 +15,26 @@
 ; Tests for functions existence
 ;
 
+(deftest test-send-drawing-existence
+    "Check that the chainring-service.vector-drawing/send-drawing definition exists."
+    (testing "if the chainring-service.vector-drawing/send-drawing definition exists."
+        (is (callable? 'chainring-service.vector-drawing/send-drawing))))
+
+
+(deftest test-vector-drawing-as-drw-existence
+    "Check that the chainring-service.vector-drawing/vector-drawing-as-drw definition exists."
+    (testing "if the chainring-service.vector-drawing/vector-drawing-as-drw definition exists."
+        (is (callable? 'chainring-service.vector-drawing/vector-drawing-as-drw))))
+
+
+(deftest test-vector-drawing-as-json-existence
+    "Check that the chainring-service.vector-drawing/vector-drawing-as-json definition exists."
+    (testing "if the chainring-service.vector-drawing/vector-drawing-as-json definition exists."
+        (is (callable? 'chainring-service.vector-drawing/vector-drawing-as-json))))
+
+
+(deftest test-vector-drawing-as-binary-existence
+    "Check that the chainring-service.vector-drawing/vector-drawing-as-binary definition exists."
+    (testing "if the chainring-service.vector-drawing/vector-drawing-as-binary definition exists."
+        (is (callable? 'chainring-service.vector-drawing/vector-drawing-as-binary))))
+
