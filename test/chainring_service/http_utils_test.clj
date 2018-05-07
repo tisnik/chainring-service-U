ccf92be4c3996f062bea82855345d59859807e01
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-07 09:09:54 +0200

    Tests for module http-utils

--- test/chainring_service/http_utils_test.clj
+++ test/chainring_service/http_utils_test.clj
@@ -20,3 +20,15 @@
     (testing "if the chainring-service.http-utils/return-file definition exists."
         (is (callable? 'chainring-service.http-utils/return-file))))
 
+
+(deftest test-cache-control-headers-existence
+    "Check that the chainring-service.http-utils/cache-control-headers definition exists."
+    (testing "if the chainring-service.http-utils/cache-control-headers definition exists."
+        (is (callable? 'chainring-service.http-utils/cache-control-headers))))
+
+
+(deftest test-png-response-existence
+    "Check that the chainring-service.http-utils/png-response definition exists."
+    (testing "if the chainring-service.http-utils/png-response definition exists."
+        (is (callable? 'chainring-service.http-utils/png-response))))
+
