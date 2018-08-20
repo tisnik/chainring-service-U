6aef36fe9ff04ae1aa8373d7fb9ad1636e2ae8e3
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-20 14:20:35 +0200

    Cleaning in drawing.js - all Raphael relevant functions

--- www/drawing.js
+++ www/drawing.js
@@ -164,94 +164,6 @@ function callAjax(url, callback) {
     xmlHttpRequest.send();
 }
 
-function onLoadDrawing(raw_data) {
-    var lines = raw_data.split("\n");
-    drawing_input = lines;
-    window.alert(lines.length);
-    var length = lines.length;
-    for (var i = 0; i < length; i++) {
-        var line = lines[i];
-    }
-}
-
-function onLoadDrawingJSON(raw_data) {
-    //drawing = JSON.parse(raw_data)
-    if (typeof JSON == 'object') {
-        if (debugMode) {
-            console.log("using JSON.parse() to deserialize drawing");
-        }
-        drawing = JSON.parse(raw_data);
-    }
-    else {
-        console.log("using eval() to deserialize drawing");
-        drawing = eval("(" + raw_data + ")");
-    }
-    var scales = drawing["scales"];
-    var scale = 0, xoffset = 0, yoffset = 0;
-    for (i=0; i<scales.length; i++) {
-        if ((scales[i]["width"] == 800) && (scales[i]["height"] = 600)) {
-            scale = scales[i]["scale"];
-            xoffset = scales[i]["xoffset"];
-            yoffset = scales[i]["yoffset"];
-        }
-    }
-    console.log("scale", scale);
-    console.log("xoffset", xoffset);
-    console.log("yoffset", yoffset);
-    var entities = drawing["entities"];
-    for (i=0; i<entities.length; i++) {
-        var entity = entities[i];
-        switch (entity["T"]) {
-            case "L":
-                var x1 = (entity["x1"] + xoffset) * scale;
-                var y1 = (entity["y1"] + yoffset) * scale;
-                var x2 = (entity["x2"] + xoffset) * scale;
-                var y2 = (entity["y2"] + yoffset) * scale;
-                var path = "M" + x1 + "," + y1 + " L" + x2 + "," + y2;
-                paper.path(path);
-                break;
-            case "A":
-                var x = (entity["x"] + xoffset) * scale;
-                var y = (entity["y"] + yoffset) * scale;
-                var r = entity["r"] * scale;
-                var a1 = entity["a1"];
-                var a2 = entity["a2"];
-                paper.circularArc(x, y, r, a1, a2);
-                break;
-            case "T":
-                var x = (entity["x"] + xoffset) * scale;
-                var y = (entity["y"] + yoffset) * scale;
-                var text = entity["text"];
-                paper.text(x, y, text).attr("font-size", 7);//.attr("stroke", "#0000FF");
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-function initializePaper(elementId, width, height) {
-    paper = Raphael(document.getElementById(elementId), width, height);
-    paper.clear();
-}
-
-function downloadDrawingById(drawingId) {
-    var url = "/vector-drawing-as-drw?drawing-id=" + drawingId;
-    callAjax(url, onLoadDrawing);
-}
-
-function downloadJSONDrawingById(drawingId) {
-    var url = "/vector-drawing-as-json?drawing-id=" + drawingId;
-    console.log("Loading drawing with id=" + drawingId);
-    callAjax(url, onLoadDrawingJSON);
-}
-
-function downloadJSONDrawingByName(drawingName) {
-    var url = "/vector-drawing-as-json?drawing-name=" + drawingName;
-    console.log("Loading drawing with name=" + drawingName);
-    callAjax(url, onLoadDrawingJSON);
-}
-
 function getEvent(e)
 {
     return !e ? window.event : e;
@@ -482,14 +394,6 @@ function roomOccupiedByCheckBoxClicked() {
 window.onload = function () {
     // drawGrid
     console.log(version);
-    if (drawing_id !== null) {
-        initializePaper("drawing_canvas", 800, 600);
-        downloadJSONDrawingById(drawing_id);
-    }
-    else if (drawing_name !== null) {
-        initializePaper("drawing_canvas", 800, 600);
-        downloadJSONDrawingByName(drawing_name);
-    }
 };
 
 function showHideSomething(elementId, iconId, hide) {
