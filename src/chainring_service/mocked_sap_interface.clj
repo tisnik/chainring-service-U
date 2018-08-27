6bee17239a71716cf471df0974080f7cbf1fd3d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-27 12:09:16 +0200

    Loading test CSV files for the mocked SAP interface

--- src/chainring_service/mocked_sap_interface.clj
+++ src/chainring_service/mocked_sap_interface.clj
@@ -12,7 +12,71 @@
 
 (ns chainring-service.mocked-sap-interface)
 
+(require '[clojure.data.csv :as csv])
+(require '[clojure.java.io  :as io])
+
+(require '[clojure.tools.logging      :as log])
+
+
+(def areals-data-file
+    "data/areals.csv")
+
+(def buildings-data-file
+    "data/buildings.csv")
+
+(def floors-data-file
+    "data/floors.csv")
+
+(def rooms-data-file
+    "data/rooms.csv")
+
+(def areals
+    (atom nil))
+
+(def buildings
+    (atom nil))
+
+(def floors
+    (atom nil))
+
+(def rooms
+    (atom nil))
+
+(defn csv-data->maps
+    [csv-data]
+    (map zipmap
+        (->> (first csv-data)  ;; header
+             (map keyword)     ;; heder items -> keywords
+             repeat)
+             (rest csv-data)))
+
+(defn load-csv
+    [filename]
+    (with-open [reader (io/reader filename)]
+        (let [data (csv/read-csv reader)]
+             (doall (csv-data->maps data)))))
+
+(defn load-all-data-files
+    []
+    (reset! areals    (load-csv areals-data-file))
+    (reset! buildings (load-csv buildings-data-file))
+    (reset! floors    (load-csv floors-data-file))
+    (reset! rooms     (load-csv rooms-data-file)))
+
+(load-all-data-files)
+
+(defn reload-mock-data
+    []
+    (log/info "Reload mock data begin")
+    (load-all-data-files)
+    (let [status {:areals    (count @areals)
+                  :buildings (count @buildings)
+                  :floors    (count @floors)
+                  :rooms     (count @rooms)}]
+        (log/info "Reload mock data end")
+        status))
+
 (defn read-areals
     []
-    [1 2 3 4]
+    @areals
     )
