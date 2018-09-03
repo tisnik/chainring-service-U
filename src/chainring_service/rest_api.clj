78a32e43189dc8b4d759084e960a69811f5c409c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-03 10:11:28 +0200

    Refactoring: REST API utils module

--- src/chainring_service/rest_api.clj
+++ src/chainring_service/rest_api.clj
@@ -16,11 +16,8 @@
     Author: Pavel Tisnovsky")
 
 
-(require '[ring.util.response         :as http-response])
 (require '[clojure.pprint             :as pprint])
-(require '[clojure.data.json          :as json])
 (require '[clojure.tools.logging      :as log])
-(require '[clj-fileutils.fileutils    :as file-utils])
 
 (require '[chainring-service.db-interface     :as db-interface])
 (require '[chainring-service.config           :as config])
@@ -29,82 +26,22 @@
 
 (require '[chainring-service.sap-interface         :as sap-interface])
 (require '[chainring-service.mocked-sap-interface  :as mocked-sap-interface])
+(require '[chainring-service.rest-api-utils        :as rest-api-utils])
 
 (use     '[clj-utils.utils])
 
 
-; HTTP codes used by several REST API responses
-(def http-codes
-    "HTTP codes used by several REST API responses."
-    {
-    :ok                    200
-    :bad-request           400
-    :not-found             404
-    :internal-server-error 500
-    :not-implemented       501})
-
-
-(defn read-request-body
-    "Read all informations from the request body."
-    [request]
-    (file-utils/slurp- (:body request)))
-
-
-(defn body->results
-    "Try to parse the request body as JSON format."
-    [body]
-    (json/read-str body))
-
-
-(defn send-response
-    "Send normal response (with application/json MIME type) back to the client."
-    ([response request http-code]
-     (if (config/pretty-print? request)
-         (-> (http-response/response (with-out-str (json/pprint response)))
-             (http-response/content-type "application/json")
-             (http-response/status (get http-codes http-code)))
-         (-> (http-response/response (json/write-str response))
-             (http-response/content-type "application/json")
-             (http-response/status (get http-codes http-code)))))
-    ([response request]
-     (send-response response request :ok)))
-
-
-(defn send-ok-response
-    "Send ok response (with application/json MIME type) back to the client."
-    [message request]
-    (let [response {:status "ok"
-                    :message message}]
-        (send-response response request :ok)))
-
-
-(defn send-error-response
-    "Send error response (with application/json MIME type) back to the client."
-    [message cause request http-code]
-    (let [response {:status "error"
-                    :message message
-                    :cause cause}]
-        (send-response response request http-code)))
-
-
-(defn send-plain-response
-    "Send a response (with application/json MIME type) back to the client."
-    [response]
-    (-> (http-response/response response)
-        (http-response/content-type "application/json")))
-
-
-(defn unknown-endpoint
+(defn unknown-endpoint-handler
     "Process any unknown endpoints."
     [request uri]
-    (send-error-response "unknown endpoint" uri request :bad-request))
+    (rest-api-utils/send-error-response "unknown endpoint" uri request :bad-request))
 
 
 (defn toplevel-handler
     "REST API handler for the /api endpoint."
     [request api-full-prefix]
     (let [response {api-full-prefix "current REST API version endpoint"}]
-        (send-response response request)))
+        (rest-api-utils/send-response response request)))
 
 
 (defn api-info-handler
@@ -120,7 +57,7 @@
                     (str api-prefix "/building")       "building metadata"
                     (str api-prefix "/drawing")        "drawing metadata"
                     (str api-prefix "/drawings-cache") "drawings cache statistic"}]
-        (send-response response request)))
+        (rest-api-utils/send-response response request)))
 
 
 (defn info-handler
@@ -133,28 +70,28 @@
                     :api-version     (config/get-api-version request)
                     :full-prefix     (config/get-api-full-prefix request)}]
                     ;:hostname   hostname :test "/api"}]
-        (send-response response request)))
+        (rest-api-utils/send-response response request)))
 
 
 (defn liveness-handler
     "REST API handler for the /api/{version}/liveness endpoint."
     [request]
     (let [response {:status "ok"}]
-        (send-response response request)))
+        (rest-api-utils/send-response response request)))
 
 
 (defn readiness-handler
     "REST API handler for the /api/{version}/readiness endpoint."
     [request]
     (let [response {:status "ok"}]
-        (send-response response request)))
+        (rest-api-utils/send-response response request)))
 
 
 (defn config-handler
     "REST API handler for the /api/{version}/config endpoint."
     [request]
     (let [response {:configuration (:configuration request)}]
-        (send-response response request)))
+        (rest-api-utils/send-response response request)))
 
 
 (defn project-list-handler
@@ -163,8 +100,8 @@
     (let [projects      (db-interface/read-project-list)]
         (log/info "Projects:" projects)
         (if projects
-            (send-response projects request)
-            (send-error-response "database access error" uri request :internal-server-error))))
+            (rest-api-utils/send-response projects request)
+            (rest-api-utils/send-error-response "database access error" uri request :internal-server-error))))
 
 
 (defn read-project-info
@@ -201,8 +138,8 @@
     (let [params       (:params request)
           project-id   (get params "project-id")]
           (if project-id
-              (send-response (read-project-info project-id) request)
-              (send-error-response "you need to specify project ID" uri request :internal-server-error))))
+              (rest-api-utils/send-response (read-project-info project-id) request)
+              (rest-api-utils/send-error-response "you need to specify project ID" uri request :internal-server-error))))
 
 
 (defn building-handler
@@ -211,8 +148,8 @@
     (let [params        (:params request)
           building-id   (get params "building-id")]
           (if building-id
-              (send-response (read-building-info building-id) request)
-              (send-error-response "you need to specify building ID" uri request :internal-server-error))))
+              (rest-api-utils/send-response (read-building-info building-id) request)
+              (rest-api-utils/send-error-response "you need to specify building ID" uri request :internal-server-error))))
 
 
 (defn floor-handler
@@ -226,8 +163,8 @@
           (if floor-id
               (let [drawings (db-interface/read-drawing-list floor-id)]
                   (log/info "Drawings" drawings)
-                  (send-response drawings request))
-              (send-error-response "you need to specify floor ID" uri request :internal-server-error))))
+                  (rest-api-utils/send-response drawings request))
+              (rest-api-utils/send-error-response "you need to specify floor ID" uri request :internal-server-error))))
 
 
 (defn drawing-handler
@@ -244,9 +181,9 @@
           (log/info "Rooms" rooms)
           (if drawing-id
               (if drawing-info
-                  (send-response result request)
-                  (send-error-response "no drawing info" uri request :internal-server-error))
-              (send-error-response "you need to specify drawing ID" uri request :internal-server-error))))
+                  (rest-api-utils/send-response result request)
+                  (rest-api-utils/send-error-response "no drawing info" uri request :internal-server-error))
+              (rest-api-utils/send-error-response "you need to specify drawing ID" uri request :internal-server-error))))
 
 
 ; TODO: filters
@@ -255,7 +192,7 @@
     [request uri]
     (let [params    (:params request)
           projects  (db-interface/read-project-list)]
-         (send-response projects request)))
+         (rest-api-utils/send-response projects request)))
 
 
 (defn all-buildings
@@ -263,7 +200,7 @@
     [request uri]
     (let [params    (:params request)
           buildings (db-interface/read-all-buildings)]
-         (send-response buildings request)))
+         (rest-api-utils/send-response buildings request)))
 
 
 (defn all-floors
@@ -271,7 +208,7 @@
     [request uri]
     (let [params    (:params request)
           floors    (db-interface/read-all-floors)]
-         (send-response floors request)))
+         (rest-api-utils/send-response floors request)))
 
 
 (defn sap-reload-mock-data
@@ -280,7 +217,7 @@
           mock-sap-response (config/mock-sap-response? request)]
           (if mock-sap-response
               (let [status (mocked-sap-interface/reload-mock-data)]
-                    (send-response status request)))))
+                    (rest-api-utils/send-response status request)))))
 
 
 (defn get-sap-href
@@ -304,13 +241,13 @@
     (let [configuration (-> request :configuration :sap-interface)
           object-type   (get-sap-selector uri)
           href          (get-sap-href configuration object-type)]
-          (send-response href request)))
+          (rest-api-utils/send-response href request)))
 
 
 (defn sap-areals
     [request uri]
     (let [sap-response (sap-interface/call-sap-interface request "read-areals")]
-        (send-response sap-response request)))
+        (rest-api-utils/send-response sap-response request)))
 
 
 (defn sap-buildings
@@ -318,7 +255,7 @@
     (let [params            (:params request)
           areal             (get params "areal")
           sap-response      (sap-interface/call-sap-interface request "read-buildings" areal)]
-        (send-response sap-response request)))
+        (rest-api-utils/send-response sap-response request)))
 
 
 (defn sap-floors
@@ -327,7 +264,7 @@
           areal             (get params "areal")
           building          (get params "building")
           sap-response      (sap-interface/call-sap-interface request "read-floors" areal building)]
-        (send-response sap-response request)))
+        (rest-api-utils/send-response sap-response request)))
 
 
 (defn sap-rooms
@@ -337,7 +274,7 @@
           building          (get params "building")
           floor             (get params "floor")
           sap-response      (sap-interface/call-sap-interface request "read-rooms" areal building floor)]
-        (send-response sap-response request)))
+        (rest-api-utils/send-response sap-response request)))
 
 
 (defn sap-debug-handler
@@ -346,7 +283,7 @@
     (let [params    (:params request)]
          (log/info "fake SAP handler")
          (log/info params)
-         (send-response params request)))
+         (rest-api-utils/send-response params request)))
 
 (defn read-attributes-for-rooms
     [attribute]
@@ -355,7 +292,7 @@
 (defn sap-room-attributes
     [request uri]
     (let [attributes ["kapacita" "obsazenost" "typ" "plocha"]]
-        (send-response attributes request)
+        (rest-api-utils/send-response attributes request)
     ))
 
 (defn rooms-with-attribute
@@ -367,7 +304,7 @@
           floor      (get params "floor-aoid")
           valid_from (get params "valid_from")]
           (let [rooms     (read-attributes-for-rooms attribute)]
-              (send-response rooms request)
+              (rest-api-utils/send-response rooms request)
           )))
 
 (defn all-drawings-handler
@@ -383,7 +320,7 @@
                      :floors    floors
                      :drawings  drawings}
          ]
-         (send-response response request)))
+         (rest-api-utils/send-response response request)))
 
 
 (defn drawings-cache-info-handler
@@ -391,22 +328,22 @@
     [request]
     (let [response {:cache-utilization @drawings-cache/hit-counters
                     :cache-size (drawings-cache/cache-size)}]
-         (send-response response request)))
+         (rest-api-utils/send-response response request)))
 
 (defn store-drawing-raw-data
     "REST API handler for the /api/{version}/drawing-raw-data endpoint."
     [request]
     (let [params     (:params request)
           drawing-id (get params "drawing")
-          raw-data   (read-request-body request)]
+          raw-data   (rest-api-utils/read-request-body request)]
         (if (and drawing-id raw-data)
             (try
                 (db-interface/store-drawing-raw-data drawing-id raw-data)
-                (send-ok-response "Drawing has been written into the database" request)
+                (rest-api-utils/send-ok-response "Drawing has been written into the database" request)
                 (catch Exception e
                     (log/error e)
-                    (send-error-response "exception occured during write" (.getMessage e) request :internal-server-error)))
-            (send-error-response "send drawing ID as parameter and raw data in the body" "wrong input" request :bad-request)
+                    (rest-api-utils/send-error-response "exception occured during write" (.getMessage e) request :internal-server-error)))
+            (rest-api-utils/send-error-response "send drawing ID as parameter and raw data in the body" "wrong input" request :bad-request)
         )))
 
 
@@ -414,7 +351,7 @@
     "Send error response when some required parameter is missing."
     [request parameter]
     (let [message (str "missing required parameter '" parameter "'")]
-        (send-error-response message "wrong input" request :bad-request)))
+        (rest-api-utils/send-error-response message "wrong input" request :bad-request)))
 
 
 (defn try-to-load-drawing
@@ -435,10 +372,10 @@
           (cond (and drawing-id format)
                 (try
                     (-> (try-to-load-drawing drawing-id store-format configuration)
-                        (send-response request 200))
+                        (rest-api-utils/send-response request 200))
                     (catch Exception e
                         (log/error e)
-                        (send-error-response "exception occured during write" (.getMessage e) request :internal-server-error)))
+                        (rest-api-utils/send-error-response "exception occured during write" (.getMessage e) request :internal-server-error)))
                 (nil? drawing-id)
                 (missing-parameter request "drawing")
                 (nil? store-format)
@@ -461,17 +398,17 @@
           configuration (:configuration request)
           drawing-id    (get params "drawing")
           store-format  (get params "format")
-          raw-data      (read-request-body request)]
+          raw-data      (rest-api-utils/read-request-body request)]
           (cond (and drawing-id format raw-data)
                 (try
                     (try-to-store-drawing drawing-id store-format raw-data configuration)
-                    (send-ok-response "Drawing has been saved" request)
+                    (rest-api-utils/send-ok-response "Drawing has been saved" request)
                     (catch Exception e
                         (log/error e)
-                        (send-error-response "exception occured during write" (.getMessage e) request :internal-server-error)))
+                        (rest-api-utils/send-error-response "exception occured during write" (.getMessage e) request :internal-server-error)))
                 (nil? drawing-id)
                 (missing-parameter request "drawing")
                 (nil? store-format)
                 (missing-parameter request "format")
                 (nil? raw-data)
-                (send-error-response "missing body with drawing data" "wrong input" request :bad-request))))
+                (rest-api-utils/send-error-response "missing body with drawing data" "wrong input" request :bad-request))))
---
+++ src/chainring_service/rest_api_utils.clj
@@ -0,0 +1,84 @@
+;
+;  (C) Copyright 2017, 2018  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns chainring-service.rest-api-utils
+    "Utility functions used by REST API handlers.
+
+    Author: Pavel Tisnovsky")
+
+
+(require '[ring.util.response         :as http-response])
+(require '[clojure.data.json          :as json])
+(require '[clj-fileutils.fileutils    :as file-utils])
+
+(require '[chainring-service.config   :as config])
+
+
+; HTTP codes used by several REST API responses
+(def http-codes
+    "HTTP codes used by several REST API responses."
+    {
+    :ok                    200
+    :bad-request           400
+    :not-found             404
+    :internal-server-error 500
+    :not-implemented       501})
+
+
+(defn read-request-body
+    "Read all informations from the request body."
+    [request]
+    (file-utils/slurp- (:body request)))
+
+
+(defn body->results
+    "Try to parse the request body as JSON format."
+    [body]
+    (json/read-str body))
+
+
+(defn send-response
+    "Send normal response (with application/json MIME type) back to the client."
+    ([response request http-code]
+     (if (config/pretty-print? request)
+         (-> (http-response/response (with-out-str (json/pprint response)))
+             (http-response/content-type "application/json")
+             (http-response/status (get http-codes http-code)))
+         (-> (http-response/response (json/write-str response))
+             (http-response/content-type "application/json")
+             (http-response/status (get http-codes http-code)))))
+    ([response request]
+     (send-response response request :ok)))
+
+
+(defn send-ok-response
+    "Send ok response (with application/json MIME type) back to the client."
+    [message request]
+    (let [response {:status "ok"
+                    :message message}]
+        (send-response response request :ok)))
+
+
+(defn send-error-response
+    "Send error response (with application/json MIME type) back to the client."
+    [message cause request http-code]
+    (let [response {:status "error"
+                    :message message
+                    :cause cause}]
+        (send-response response request http-code)))
+
+
+(defn send-plain-response
+    "Send a response (with application/json MIME type) back to the client."
+    [response]
+    (-> (http-response/response response)
+        (http-response/content-type "application/json")))
